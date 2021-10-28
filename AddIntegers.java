b5d3951ff0b9303ee37acfce98026d2fcbeac3f7
limedd <92121941+limedd@users.noreply.github.com>
2021-10-28 17:06:05 -0400

    Update AddIntegers.java
    first to 200 second to 3000

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 20;
-        int second = 30;
+        int first = 200;
+        int second = 3000;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
